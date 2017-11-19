073698efda321f26f2bf176814f2621780f4eb68
Dhruv Govil <dhruvagovil@gmail.com>
2017-11-18 18:39:14 -0800

    Ray check against simple shere object

--- SimpleRaytracer.cpp
+++ SimpleRaytracer.cpp
@@ -11,8 +11,22 @@
 #include <fstream>
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - 4 * a*c;
+	return (discriminant > 0);
+}
+
 vec3 color(const ray& r)
 {
+	if (hit_sphere(vec3(0,0,-1), 0.5f, r))
+	{
+		return vec3(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f*(unit_direction.y() + 1.0f);
 	return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
